0f433b62f867ca5446826b47fbf93eab2d78be30
susanagonvan <susanagonvan@gmail.com>
2019-09-28 13:20:56 -0300

    Toc toc rueba again

--- ejercicios/toctoc/susanafolder/toctoc.js
+++ ejercicios/toctoc/susanafolder/toctoc.js
@@ -9,16 +9,24 @@
     await miPagina.click('#linkPackagesLayoutFooter')
  
     await miPagina.evaluate(()=> { document.querySelectorAll('.btn.big.btn-search')[3].click(); } );
- /*
-    let botones = await miPagina.$$('selecitr')
-    let mibotondeverdad = botones[3]
-    await mibotondeverdad.click()//
-
+ /* forma idem de hacer lo de la línea 11:
     await miPagina.waitForSelector('.btn.big.btn-search')
-    await miPagina.click('.btn.big.btn-search')
+    let botones = await miPagina.$$('.btn.big.btn-search')
+    let mibotondeverdad = botones[3]
+    await mibotondeverdad.click() 
     */
 
+   
+    /* probar hacer esto pero con sugerencia de Matías de linea 12:
+   await miPagina.evaluate(()=> { document.querySelectorAll('h4')[11].click(); } );
+    */
 
+    /*await miPagina.waitForSelector('h4')
+    let botones = await miPagina.$$('h4')
+    let mibotondeverdad = botones[11]
+    await mibotondeverdad.click() */
+
+    await miPagina.evaluate(()=> { document.querySelectorAll('h4')[11].click(); } );
  
         
 
