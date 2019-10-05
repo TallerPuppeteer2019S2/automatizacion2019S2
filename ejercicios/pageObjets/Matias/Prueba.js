663c6128230fc441f48b7d8ddecffd9e7ba94ffd
Matias <97maydu@gmail.com>
2019-10-05 13:00:59 -0300

    Ejercicio Rust

--- ejercicios/pageObjets/Matias/Prueba.js
+++ ejercicios/pageObjets/Matias/Prueba.js
@@ -2,14 +2,13 @@
 
 
 const MiPagina = require('./RustMain.js')
-
 const puppeteer = require('puppeteer')
 
 let miPagina = new MiPagina()
 let miNavegador = await puppeteer.launch({headless: false})
 
 await miPagina.get(miNavegador)
-await miPagina.started()
+await miPagina.printLink()
     
 
 })()
--- ejercicios/pageObjets/Matias/RustMain.js
+++ ejercicios/pageObjets/Matias/RustMain.js
@@ -1,21 +1,32 @@
 
 const selectors = {
 
-    btnGetStarted:'.button.button-download.ph4.mt0.w-100'
+    btnGetStarted: '.button.button-download.ph4.mt0.w-100',
 }
 class MiPagina {
-    constructor(){
-    this.url = 'https://www.rust-lang.org'
-    this.pagina = null
+    constructor() {
+        this.url = 'https://www.rust-lang.org'
+        this.pagina = null
     }
-    async get(browser){
+    async get(browser) {
         this.pagina = await browser.newPage()
         await this.pagina.goto(this.url)
     }
-    async started(){
+    async started() {
         await this.pagina.waitForSelector(selectors.btnGetStarted)
         await this.pagina.click(selectors.btnGetStarted)
-    }   
+    }
+    async printLink() {
+        await this.pagina.waitForSelector('.flex.flex-row.flex-wrap')
+        let iconos = (await this.pagina.$$('.flex.flex-row.flex-wrap'))[2]
+        let link = (await iconos.$$('a'))
+        const txtLink = (await link.forEach(txtLink2 => console.log(await txtLink2.getProperty(href))))
+
+
+
+
+    }
+
 }
 
-module.exports=MiPagina
+module.exports = MiPagina
